71c645b2d75bfadacf8e4cd00f41c5950220027b
Noah Meneghini <noah.meneghini@gmail.com>
2021-12-12 23:17:50 -0500

    program fix

--- Update.py
+++ Update.py
@@ -1,8 +1,7 @@
 import requests
 import json
 
-## Database functions to call in cogs ##
-
+## Update db.json ##
 
 api = requests.get('https://api.guildwars2.com/v2/commerce/prices')
 id = api.json() if api and api.status_code == 200 else None
@@ -10,34 +9,11 @@ price = dict()
 for n in id:
 	item = requests.get('https://api.guildwars2.com/v2/commerce/prices/'+str(n))
 	price[n] = item.json() if item and item.status_code == 200 else None
+	del price[n]['id']
+	del price[n]['whitelisted']
+	item = requests.get('https://api.guildwars2.com/v2/items/'+str(n))
+	apple = item.json() if item and item.status_code == 200 else None
+	price[n]['name'] = apple['name']
 	print(n)
 	with open('db.json', 'w+') as file:
 		json.dump(price, file)
-
-
-## read database ##
-def read(key, id):
-	with open('database/db.json', "r") as json_file:
-		data = json_file.read()
-	data = json.loads(data)
-	return data[key][id]
-
-## write to database ##
-def write(key, id, value):
-	with open("database/db.json", "r") as json_file:
-		data = json_file.read()
-	data = json.loads(data)
-	data[key][id] = value
-	with open("database/db.json", "w") as json_file:
-		json.dump(data, json_file)
-	return True
-
-## append to database list ##
-def append(key, id, value):
-	with open("database/db.json", "r") as json_file:
-		data = json_file.read()
-	data = json.loads(data)
-	data[key][id] = data[key][id].append(value)
-	with open("database/db.json", "w") as json_file:
-		json.dump(data, json_file)
-	return True
