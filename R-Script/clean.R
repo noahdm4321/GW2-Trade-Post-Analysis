d135ac94c74943830dda8734d892793495d23fdf
Noah Meneghini <noahdm4321@users.noreply.github.com>
2023-07-09 12:44:04 -0400

    R update for vscode

--- R-Script/clean.R
+++ R-Script/clean.R
@@ -1,24 +1,28 @@
 # Load necessary packages
-install.packages("tidyverse")
-install.packages("RSQLite")
+# install.packages("tidyverse")
+# install.packages("RSQLite")
 
 library(tidyverse)
 library(RSQLite)
 
-#################################################################################
+################################################################################
 
 # Connect to the database
-con <- dbConnect(SQLite(), dbname = "../data.db")
+con <- dbConnect(SQLite(), dbname = "data.db")
 
 # Query the raw data from database
 query <- "SELECT [id], [name], [value] FROM [items]"
 items <- dbGetQuery(con, query)
-query <- "SELECT [item_id], [buy_price], [buy_quantity], [sell_price], [sell_quantity] FROM [prices]"
+query <- "SELECT [item_id], [buy_price], [buy_quantity], [sell_price], 
+  [sell_quantity] FROM [prices]"
 prices <- dbGetQuery(con, query)
-query <- "SELECT [output_id], [output_count], [input1_id], [input1_count], [input2_id], [input2_count], [input3_id], [input3_count], [input4_id], [input4_count] FROM [recipes]"
+query <- "SELECT [output_id], [output_count], [input1_id], [input1_count], 
+  [input2_id], [input2_count], [input3_id], [input3_count], [input4_id], 
+  [input4_count] FROM [recipes]"
 recipes <- dbGetQuery(con, query)
+rm(query)
 
-#################################################################################
+################################################################################
 
 # Define item_data dataframe
 item_data <- merge(
@@ -36,13 +40,15 @@ item_data$sell_quantity[is.na(item_data$sell_quantity)] <- 0
 item_data$buy_quantity[is.na(item_data$buy_quantity)] <- 0
 item_data <- subset(item_data, select = -value)
 # Add profit column
-item_data$profit <- item_data$sell_price-1 - ceiling((item_data$buy_price+1) * 0.85)
+item_data$profit <- item_data$sell_price-1 -
+  ceiling((item_data$buy_price+1) * 0.85)
 # Add profit column for buy now and sell now
-item_data$profit_now <- ceiling((item_data$buy_price) * 0.85) - item_data$sell_price
+item_data$profit_now <- ceiling((item_data$buy_price) * 0.85) -
+  item_data$sell_price
 
-#################################################################################
+################################################################################
 
-# Create dataframe of investment oportunities
+# Create dataframe of investment opportunities
 investments <- item_data %>%
   # Filter out items that are not profitable
   filter(profit > 0, sell_quantity > 0, buy_quantity > 0) %>%
@@ -57,11 +63,11 @@ ggplot(data = investments, mapping = aes(x=sell_price, y=profit)) +
   # Add maximum profit line
   geom_abline(slope = 1, intercept = 0, color = "red")
 
-### Remove irrelevant items: items that have listings which would never sell. ###
+### Remove irrelevant items: items that have listings which would never sell. ##
 
 # Plot irrelevant items
 ggplot(data = investments, mapping = aes(x=sell_price, y=buy_price)) +
-  geom_point() + 
+  geom_point() +
   # Add deal line: line at which items will sell/buy
   geom_abline(slope = 1, intercept = 0, color = "red")
 
@@ -74,9 +80,9 @@ investments <- investments %>%
 ggplot(data = investments, mapping = aes(x=sell_price, y=buy_price)) +
   geom_point() + 
   # Add deal line: line at which items will sell/buy
-  geom_abline(slope = 1, intercept = 0, color = "red")  
-  
-################################# Finalize filter ###############################
+  geom_abline(slope = 1, intercept = 0, color = "red")
+
+################################# Finalize filter ##############################
 
 # Refine investments
 investments <- investments %>%
@@ -93,16 +99,16 @@ ggplot(data = investments, mapping = aes(x=sell_price, y=profit)) +
 
 print(investments %>% arrange(desc(profit)))
 
-#################################################################################
+################################################################################
 
-# Create dataframe of investment_now oportunities
+# Create dataframe of investment_now opportunities
 investments_now <- item_data %>%
   # Filter out items that are not profitable
   filter(profit_now > 0, sell_quantity > 0, buy_quantity > 0)
 
-print(investments_now %>% arrange(desc(profit_now)))  #typically blank
+# print(investments_now %>% arrange(desc(profit_now)))  #typically blank
 
-#################################################################################
+################################################################################
 
 # # Create empty dataframe with desired structure
 # recipe_data <- data.frame(
@@ -114,10 +120,10 @@ print(investments_now %>% arrange(desc(profit_now)))  #typically blank
 #   input_prices = I(list()),
 #   stringsAsFactors = FALSE
 # )
-# 
+#
 # # Pre-allocate rows in the dataframe
 # recipe_data <- recipe_data[rep(NA, nrow(recipes)), ]
-# 
+#
 # # Iterate over each row in 'recipes' dataframe
 # for (i in 1:nrow(recipes)) {
 #   new_row <- data.frame(
@@ -126,7 +132,7 @@ print(investments_now %>% arrange(desc(profit_now)))  #typically blank
 #     output_count = recipes[i, "output_count"],
 #     stringsAsFactors = FALSE
 #   )
-#   
+#
 #   # Form input_ids as nested list
 #   input_ids <- list(
 #     as.character(recipes[i, "input1_id"]),
@@ -134,7 +140,7 @@ print(investments_now %>% arrange(desc(profit_now)))  #typically blank
 #     as.character(recipes[i, "input3_id"]),
 #     as.character(recipes[i, "input4_id"])
 #   )
-#   
+#
 #   # Form input_counts as nested list
 #   input_counts <- list(
 #     as.integer(recipes[i, "input1_count"]),
@@ -142,28 +148,32 @@ print(investments_now %>% arrange(desc(profit_now)))  #typically blank
 #     as.integer(recipes[i, "input3_count"]),
 #     as.integer(recipes[i, "input4_count"])
 #   )
-#   
+#
 #   # Form input_prices as nested list
 #   input_prices <- list(
 #     item_data[recipes[i, "input1_id"], "buy_price"],
-#     ifelse(is.na(recipes[i, "input2_id"]), NA, item_data[recipes[i, "input2_id"], "buy_price"]),
-#     ifelse(is.na(recipes[i, "input3_id"]), NA, item_data[recipes[i, "input3_id"], "buy_price"]),
-#     ifelse(is.na(recipes[i, "input4_id"]), NA, item_data[recipes[i, "input4_id"], "buy_price"])
+#     ifelse(is.na(recipes[i, "input2_id"]), NA,
+#       item_data[recipes[i, "input2_id"], "buy_price"]),
+#     ifelse(is.na(recipes[i, "input3_id"]), NA,
+#       item_data[recipes[i, "input3_id"], "buy_price"]),
+#     ifelse(is.na(recipes[i, "input4_id"]), NA,
+#       item_data[recipes[i, "input4_id"], "buy_price"])
 #   )
-#   
+#
 #   # Assign nested lists to the new_row dataframe
 #   new_row$input_ids <- input_ids
 #   new_row$input_counts <- input_counts
 #   new_row$input_prices <- input_prices
-#   
+#
 #   # Assign new_row to the corresponding row in recipe_data
 #   recipe_data[i, ] <- new_row
 # }
 
-#################################################################################
+################################################################################
 
 # Save the cleaned data to database
-dbWriteTable(con, "investments", clean, overwrite = TRUE)
+# dbWriteTable(con, "investments", clean, overwrite = TRUE)
 
 # Close the database connection
 dbDisconnect(con)
+rm(con)
